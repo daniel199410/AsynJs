1ff2c010b2187fb941320ad125969ae3bea0e154
Daniel Cataño Restrepo <dasniel199410@gmail.com>
2017-11-28 18:13:55 -0500

    Tarea 1: Stop Watch
    Consiste en tres botones:
    ---Start/Stop: Inicia/pausa/resume un cronómetro
    ---Reset: Reinicia el cronómetro y elimina los registros
    ---Record time: Almacena el tiempo en el que se hizo click en el botón

---
+++ StopWatchApp/stopWatch.js
@@ -0,0 +1,62 @@
+var stopped = true
+var count = 0
+var interval
+var startButton = document.getElementById("start")
+var resetButton = document.getElementById("reset")
+var recordButton = document.getElementById("record")
+var green = "#4CAF50"
+var red = "#F44336"
+
+setUp()
+
+function deleteRecords(){
+    document.getElementById("records").innerHTML = ""
+}
+
+function setUp(){
+    startButton.addEventListener("click", function(){
+        if(stopped)
+            start()
+        else
+            stop()
+    })
+    resetButton.addEventListener("click", restart)
+    recordButton.addEventListener("click", function(){
+        var node = document.createElement("p")
+        var textNode = document.createTextNode(count.toFixed(2))
+        node.appendChild(textNode)
+        document.getElementById("records").appendChild(node)
+    })
+    document.addEventListener("keydown", function(event){
+        if(event.key == "s")
+            startButton.click()
+        if(event.key == "r")
+            resetButton.click()
+        if(event.key == "t")
+            recordButton.click()
+    })
+}
+
+function restart(){
+    count = 0
+    document.getElementById("timer").innerHTML = 0
+    deleteRecords()
+    stop()
+}
+
+function start(){
+    stopped = false
+    startButton.innerHTML = "Stop"
+    startButton.style.backgroundColor = red
+    interval = setInterval(function(){
+        count += 0.05
+        document.getElementById("timer").innerHTML = count.toFixed(2)
+    }, 50)
+}
+
+function stop(){
+    startButton.innerHTML = "Start"
+    startButton.style.backgroundColor = green
+    clearInterval(interval)
+    stopped = true
+}
